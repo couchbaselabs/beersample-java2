e0a61f54cb6198866a7f1f9a38bbdb03af2e93a1
Simon Baslé <simon.basle@gmail.com>
2015-02-03 10:33:46 +0100

    added preDestroy hook to show cluster disconnect

--- src/main/java/com/couchbase/beersample/CouchbaseService.java
+++ src/main/java/com/couchbase/beersample/CouchbaseService.java
@@ -23,6 +23,8 @@ package com.couchbase.beersample;
 
 import java.util.List;
 
+import javax.annotation.PreDestroy;
+
 import com.couchbase.beersample.config.Database;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
@@ -57,14 +59,22 @@ public class CouchbaseService {
     private final Database config;
 
     private final Bucket bucket;
+    private final Cluster cluster;
 
     @Autowired
     public CouchbaseService(final Database config) {
         this.config = config;
 
         //connect to the cluster and open the configured bucket
-        Cluster cluster = CouchbaseCluster.create(config.getNodes());
+        this.cluster = CouchbaseCluster.create(config.getNodes());
         this.bucket = cluster.openBucket(config.getBucket(), config.getPassword());
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (this.cluster != null) {
+            this.cluster.disconnect();
+        }
     }
 
     /**
